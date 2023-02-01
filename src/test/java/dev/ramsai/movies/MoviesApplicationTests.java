e5c21d744a4e0367d0e81cc8ad1d4185814e5347
Ramsai <ramsaicrony@gmail.com>
2023-02-01 20:58:03 +0530

    Completed basic Backend API's using springBoot

---
+++ src/test/java/dev/ramsai/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ramsai.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
